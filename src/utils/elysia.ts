06657c36354523b3a640b393482a9febcd34f248
Juan Antonio <juan@dealjava.com>
2024-10-22 10:34:43 +0700

    initial: create bun & elysia project

--- src/utils/elysia.ts
+++ src/utils/elysia.ts
@@ -1,6 +1,5 @@
 import { Elysia } from 'elysia';
-import { env } from '../env';
 
 export const createElysia = (
   config?: ConstructorParameters<typeof Elysia>[0]
-) => new Elysia({ ...config, aot: env.RUNTIME === 'bun' });
+) => new Elysia(config);
