8d095d1dbf58c5815e90651a84aa361c7137a278
Juan Antonio <juan@dealjava.com>
2024-10-22 10:39:16 +0700

    initial: create bun & elysia project

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import { app } from '../src/app';
 
-// export const config = { runtime: 'edge' };
+export const config = { runtime: 'edge' };
 
 export default async function handler(request: Request) {
   return app.fetch(request);
